253cbe6ba87ee4bbacc63ddf26b1c212ef5898d8
Esteban Blanc <esteban.blanc@lse.epita.fr>
2021-02-22 11:36:48 +0100

    toolchain: Rename extracted toolchain folder

--- activate.sh
+++ activate.sh
@@ -4,14 +4,17 @@ set -e
 TOOLCHAIN_URL='https://toolchains.bootlin.com/downloads/releases/toolchains/armv7-eabihf/tarballs/armv7-eabihf--musl--stable-2020.08-1.tar.bz2'
 TOOLCHAIN_NAME='armv7_eabihf_musl_toolchain'
 TOOLCHAIN_ARCHIVE=$TOOLCHAIN_NAME.tar.bz2
-TOOLCHAIN_DIR=''
+TOOLCHAIN_DIR=$TOOLCHAIN_NAME
 
 download_toolchain() {
     curl -o $TOOLCHAIN_ARCHIVE $TOOLCHAIN_URL
 }
 
 extract_toolchain() {
+    extract_folder=$(tar -tf $TOOLCHAIN_ARCHIVE | head -n 1)
     tar -xf $TOOLCHAIN_ARCHIVE
+
+    mv $extract_folder $TOOLCHAIN_DIR
 
     rm "$TOOLCHAIN_ARCHIVE"
 }
