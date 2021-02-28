dca1168d97e942181a6fa84d2e9fbc00003f5681
Esteban Blanc <estblcsk@gmail.com>
2021-02-28 19:24:47 +0100

    rust: Working hello module

--- rust/mfrc522.c
+++ rust/mfrc522.c
@@ -1,5 +1,5 @@
-#include <linux/module.h>
 #include <linux/init.h>
+#include <linux/module.h>
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("ks0n");
@@ -8,13 +8,11 @@ MODULE_DESCRIPTION("Driver for the MFRC522 RFID Chip");
 extern int mfrc522_init_rs(void);
 extern void mfrc522_exit_rs(void);
 
-static int __init mfrc522_init(void) {
-    return mfrc522_init_rs();
-}
+static int __init mfrc522_init(void) { return mfrc522_init_rs(); }
 
-static void __exit mfrc522_exit(void) {
-    mfrc522_exit_rs();
-}
+static void __exit mfrc522_exit(void) { mfrc522_exit_rs(); }
+
+void print(char *msg) { printk(KERN_INFO "%s", msg); }
 
 module_init(mfrc522_init);
 module_exit(mfrc522_exit);
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,15 +1,19 @@
 #![no_std]
 
-#[crate_type = "staticlib"]
+extern "C" {
+    fn print(msg: *const u8) -> ();
+}
 
 #[no_mangle]
+// #[link_section = ".init"]
 pub extern "C" fn mfrc522_init_rs() -> i32 {
-    return 1
+    unsafe { print("Hello from Rust\n\0".as_bytes().as_ptr()) };
+    return 0;
 }
 
 #[no_mangle]
-pub extern "C" fn mfrc522_exit_rs() {
-}
+// #[link_section = ".exit"]
+pub extern "C" fn mfrc522_exit_rs() {}
 
 #[panic_handler]
 fn my_panic(_info: &core::panic::PanicInfo) -> ! {
