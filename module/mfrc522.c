657b8b993c116251cf3e289922e016fbc847df9d
CohenArthur <arthur.cohen@epita.fr>
2021-03-11 19:17:31 +0100

    fmt: Add SPDX license header, use pr_info and run clang-format

--- module/mfrc522.c
+++ module/mfrc522.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0+
+
 #include <linux/module.h>
 #include <linux/init.h>
 
@@ -5,14 +7,16 @@ MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("ks0n");
 MODULE_DESCRIPTION("Driver for the MFRC522 RFID Chip");
 
-static int __init mfrc522_init(void) {
-    printk(KERN_INFO "MFRC522 init\r\n");
+static int __init mfrc522_init(void)
+{
+	pr_info("MFRC522 init\r\n");
 
-    return 0;
+	return 0;
 }
 
-static void __exit mfrc522_exit(void) {
-    printk(KERN_INFO "MFRC522 exit\r\n");
+static void __exit mfrc522_exit(void)
+{
+	pr_info("MFRC522 exit\r\n");
 }
 
 module_init(mfrc522_init);
