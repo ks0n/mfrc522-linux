e9ccaefabc05fbf984c41f6311cdd6a93cc1b8ad
CohenArthur <arthur.cohen@epita.fr>
2021-03-23 22:17:27 +0100

    mem_read: Return read size on command execution
    Co-authored-by: Skallwar <esteban.blanc@epita.fr>
    Co-authored-by: n1tram1 <martin.schmidt@epita.fr>

--- module/mfrc522_user_command.c
+++ module/mfrc522_user_command.c
@@ -38,40 +38,41 @@ int mfrc522_command_simple_init(struct mfrc522_command *cmd, u8 cmd_byte)
 /**
  * Read the internal memory of the MFRC522
  *
- * @return 0 on success, -1 on error
+ * @return The size of the read on success, -1 on error
  */
-// FIXME: Use answer to return data to module?
-static int mem_read(void) {
-	u8 answer_buf[MFRC522_MAX_FIFO_LEN] = { 0 };
+static int mem_read(char *answer)
+{
 	int byte_amount;
 
 	mfrc522_fifo_flush();
-	mfrc522_send_command(MFRC522_RCV_ON, MFRC522_POWER_DOWN_OFF, MFRC522_COMMAND_MEM);
-	byte_amount = mfrc522_fifo_read(answer_buf);
+	mfrc522_send_command(MFRC522_RCV_ON, MFRC522_POWER_DOWN_OFF,
+			     MFRC522_COMMAND_MEM);
+	byte_amount = mfrc522_fifo_read(answer);
 	if (byte_amount < 0) {
 		pr_err("[MFRC522] An error happened when reading MFRC522's internal memory\n");
 		return -1;
 	}
 
-	pr_info("\"%*.s\"\n", byte_amount, answer_buf);
+	pr_info("[MFRC522] Read %d bytes from memory\n", byte_amount);
 
-	return 0;
+	return byte_amount;
 }
 
 int mfrc522_execute(char *answer, struct mfrc522_command *cmd)
 {
+	int ret = -1;
+
 	switch (cmd->cmd) {
 	case MFRC522_CMD_GET_VERSION:
-		sprintf(answer, "%d", mfrc522_get_version());
+		ret = sprintf(answer, "%d", mfrc522_get_version());
 		break;
 	case MFRC522_CMD_MEM_READ:
-		if (mem_read())
-			return -1;
+		ret = mem_read(answer);
 		break;
 	default:
-		strcpy(answer, "none");
+		ret = sprintf(answer, "%s", "Command unimplemented");
 	}
 
 	// FIXME: Add logic
-	return 0;
+	return ret;
 }
