7df2f9ec8a35ceb52c3b478f649e0ab292ec7d35
CohenArthur <arthur.cohen@epita.fr>
2021-04-19 11:21:28 +0200

    parser: Add unit tests for parser

--- rust-module/mfrc522-rs/src/user_command/command.rs
+++ rust-module/mfrc522-rs/src/user_command/command.rs
@@ -1,11 +1,72 @@
-const MFRC522_MAX_DATA_LEN: usize = 25;
+const MAX_DATA_LEN: usize = 25;
 
-struct CommandArg {
-    data_len: u8,
-    data: [u8; MFRC522_MAX_DATA_LEN],
+#[repr(u8)]
+#[derive(Debug, PartialEq)]
+pub enum Cmd {
+    MemWrite,
+    MemRead,
+    GetVersion,
+    GenRand,
 }
 
+#[derive(Debug, PartialEq)]
+struct CommandArg {
+    data_len: u8,
+    data: [u8; MAX_DATA_LEN],
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Command {
-    cmd: u8,
+    cmd: Cmd,
     arg: Option<CommandArg>,
+}
+
+impl CommandArg {
+    pub fn new(data_len: u8, data: [u8; MAX_DATA_LEN]) -> CommandArg {
+        CommandArg {
+            data_len,
+            data
+        }
+    }
+}
+
+impl Command {
+    pub fn new(cmd: Cmd, data_len: u8, data: [u8; MAX_DATA_LEN]) -> Command {
+        Command {
+            cmd,
+            arg: Some(CommandArg::new(data_len, data)),
+        }
+    }
+
+    pub fn new_simple(cmd: Cmd) -> Command {
+        Command {
+            cmd,
+            arg: None,
+        }
+    }
+
+    fn mem_write(&self) -> Option<String> {
+        todo!()
+    }
+
+    fn mem_read(&self) -> Option<String> {
+        todo!()
+    }
+
+    fn get_version(&self) -> Option<String> {
+        todo!()
+    }
+
+    fn generate_random_id(&self) -> Option<String> {
+        todo!()
+    }
+
+    pub fn execute(&self) -> Option<String> {
+        match &self.cmd {
+            Cmd::MemWrite => self.mem_write(),
+            Cmd::MemRead => self.mem_read(),
+            Cmd::GetVersion => self.get_version(),
+            Cmd::GenRand => self.generate_random_id(),
+        }
+    }
 }
--- rust-module/mfrc522-rs/src/user_command/parser.rs
+++ rust-module/mfrc522-rs/src/user_command/parser.rs
@@ -1,11 +1,97 @@
-use super::Command;
+use super::command::{Cmd, Command};
+
+#[derive(Debug, PartialEq)]
+pub enum ParseError {
+    UnknownCommand,
+    InvalidArgNumber,
+}
 
 pub struct Parser;
 
 impl Parser {
-    pub fn parse(input: &str) -> Command {
-        Command {
-            cmd: 0,
-        }
+    pub fn parse(_input: &str) -> Result<Command, ParseError> {
+        Ok(Command::new_simple(Cmd::MemRead))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_mem_read() {
+        let cmd = Parser::parse("mem_read");
+
+        assert_eq!(cmd, Ok(Command::new_simple(Cmd::MemRead)));
+    }
+
+    #[test]
+    fn valid_get_version() {
+        let cmd = Parser::parse("get_version");
+
+        assert_eq!(cmd, Ok(Command::new_simple(Cmd::GetVersion)));
+    }
+
+    #[test]
+    fn valid_gen_rand_id() {
+        let cmd = Parser::parse("gen_rand_id");
+
+        assert_eq!(cmd, Ok(Command::new_simple(Cmd::GenRand)));
+    }
+
+    #[test]
+    fn valid_mem_write() {
+        let cmd = Parser::parse("mem_write:3:Hey");
+
+        assert_eq!(cmd, Ok(Command::new_simple(Cmd::MemWrite)));
+    }
+
+    #[test]
+    fn invalid_mem_write() {
+        let cmd = Parser::parse("mem_write");
+
+        assert!(cmd.is_err());
+    }
+
+    #[test]
+    fn invalid_mem_read() {
+        let cmd = Parser::parse("mem_read:3:Hey");
+
+        assert!(cmd.is_err());
+    }
+
+    #[test]
+    fn invalid_len_arg() {
+        let cmd = Parser::parse("mem_write:NotANumber:Hey");
+
+        assert!(cmd.is_err());
+    }
+
+    #[test]
+    fn invalid_len_arg_float() {
+        let cmd = Parser::parse("mem_write:3.0:Hey");
+
+        assert!(cmd.is_err());
+    }
+
+    #[test]
+    fn invalid_len_arg_negative() {
+        let cmd = Parser::parse("mem_write:-15:Hey");
+
+        assert!(cmd.is_err());
+    }
+
+    #[test]
+    fn invalid_len_arg_too_big() {
+        let cmd = Parser::parse("mem_write:3599:Hey");
+
+        assert!(cmd.is_err());
+    }
+
+    #[test]
+    fn invalid_cmd() {
+        let cmd = Parser::parse("not_a_cmd");
+
+        assert!(cmd.is_err());
     }
 }
