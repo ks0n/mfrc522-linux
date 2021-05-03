26bd501cdb3dcba7e3ccfd1705894652637ccc30
CohenArthur <arthur.cohen@epita.fr>
2021-05-03 16:26:28 +0200

    rust-module: Fixup names and log messages

--- rust-module/mfrc522.rs
+++ rust-module/mfrc522.rs
@@ -7,13 +7,11 @@ mod command;
 mod parser;
 
 use alloc::boxed::Box;
-use core::pin::Pin;
 use kernel::prelude::*;
 use kernel::{
     chrdev, cstr,
     file_operations::{FileOpener, FileOperations},
-    spi,
-    spi_method,
+    spi, spi_method,
 };
 
 module! {
@@ -28,7 +26,7 @@ module! {
 
 spi_method! {
     fn mfrc522_probe(spi_device: SpiDevice) -> KernelResult {
-        pr_info!("Rust SPI is ON!!\n");
+        pr_info!("[MFRC522-RS] SPI Registered\n");
 
         Err(kernel::Error::from_kernel_errno(-1))
     }
@@ -38,7 +36,7 @@ struct Mfrc522FileOps;
 
 impl FileOpener<()> for Mfrc522FileOps {
     fn open(_ctx: &()) -> KernelResult<Self::Wrapper> {
-        pr_info!("rust file was opened!\n");
+        pr_info!("[MFRC522-RS] File opened\n");
         Ok(Box::try_new(Self)?)
     }
 }
@@ -49,29 +47,26 @@ impl FileOperations for Mfrc522FileOps {
     kernel::declare_file_operations!();
 }
 
-struct Mfrc522Driver {
-    _dev: Pin<Box<chrdev::Registration<1>>>,
-}
+struct Mfrc522Driver;
 
 impl KernelModule for Mfrc522Driver {
     fn init() -> KernelResult<Self> {
-        pr_info!("Rust character device sample (init)\n");
+        pr_info!("[MFRC522-RS] Init\n");
+
+        let mut chrdev_reg =
+            chrdev::Registration::<1>::new_pinned(cstr!("mfrc522_chrdev"), 0, &THIS_MODULE)?;
+        chrdev_reg.as_mut().register::<Mfrc522FileOps>()?;
 
         let mut spi_reg =
             spi::DriverRegistration::new(&THIS_MODULE, cstr!("mfrc522")).with_probe(mfrc522_probe);
         spi_reg.register()?;
 
-        let mut chrdev_reg =
-            chrdev::Registration::new_pinned(cstr!("rust_chrdev"), 0, &THIS_MODULE)?;
-
-        chrdev_reg.as_mut().register::<Mfrc522FileOps>()?;
-
-        Ok(Mfrc522Driver { _dev: chrdev_reg })
+        Ok(Mfrc522Driver {})
     }
 }
 
 impl Drop for Mfrc522Driver {
     fn drop(&mut self) {
-        pr_info!("Rust character device sample (exit)\n");
+        pr_info!("[MFRC522-RS] Exit\n");
     }
 }
